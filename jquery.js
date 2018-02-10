d356aa193d6a47f37bcc31063b8482b5c4f214cc
boris <borisf@uw.edu>
2018-02-10 12:40:13 -0800

    added ul, misc bar

--- jquery.js
+++ jquery.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
 	var headUls = $("#header").find("ul");
+	var miscUls = $("#miscellaneous").find("ul");
 	var proj = headUls.find("li").first();
 	var about = headUls.find("li").last();
 
+	var d = new Date(); // needed for fancy box cosmetics
+
 	headUls.hide();
 	$("#header").fadeIn("slow");
+	$("#miscellaneous").find("ul").slideDown(1000);
+	$("#miscellaneous").find("span").animate({
+		'padding-right': screen.width}, "slow"); 
 
 	$("#header").hover(function() {
 		$(this).css("background-color", "#BC95A0");
@@ -26,5 +32,14 @@ $(document).ready(function() {
 		$(this).css("background-color", "#FAF0FD");
 	}, function() {
 		$(this).css("background-color", "#BCA0D0");
+	});
+
+	miscUls.children().hover(function() {
+		$(this).css("-webkit-animation", "miscButton 2s infinite");
+		$(this).css("-moz-animation", "miscButton 2s infinite");
+	}, function() {
+		$(this).css("background-color", "#BDA0A0");
+		$(this).css("-webkit-animation", "unset");
+		$(this).css("-moz-animation", "unset");
 	});
 });
